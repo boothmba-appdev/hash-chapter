fd794b41c4e5e6070f6d8b1d7546c2032cda0064
hxie0 <xhy@chicagobooth.edu>
2021-07-07 07:15:49 +0000

    hash completed!

--- hash_find_value.rb
+++ hash_find_value.rb
@@ -7,3 +7,15 @@
 sample_hash = {:a => 100, :b => 200, :c => 300, :d => 400, :e => rand(200), :f => 600, :g => 0 }
 
 p "Enter an integer to find:"
+num = gets.chomp.to_i
+keys = sample_hash.keys
+count = 0
+keys.each do |key|
+    if sample_hash[key] == num
+        p num.to_s + " is under the key: " + key.to_s + "."
+        count = 1
+    end
+end
+if count == 0
+    p "Could not find the integer "+num.to_s
+end
--- hash_person.rb
+++ hash_person.rb
@@ -1,2 +1,5 @@
 # Ask for a name, age, and occupation; store the values in a Hash; display the Hash.
 p "Enter a name, age, and occupation separated by spaces:"
+ans = gets.chomp.split
+person = {:name => ans.at(0), :age => ans.at(1).to_i, :occupation => ans.at(2)}
+p person
